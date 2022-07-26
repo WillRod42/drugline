9a31afda2baecb06e4f3605cad9bf857a1629958
William Rodriguez <william@rodriguez1.com>
2022-07-26 11:02:37 -0700

    Add header and footer components

---
+++ drugline/src/Components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+	return (
+		<React.Fragment>
+			<div className="footer"><p>Disclaimer</p></div>
+		</React.Fragment>
+	);
+}
---
+++ drugline/src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+	return (
+		<React.Fragment>
+			<h1 className="display-1 text-center">Drug Line</h1>
+			<hr />
+		</React.Fragment>
+	);
+}
